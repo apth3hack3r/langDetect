decc22e940c4fcf3320860cd7fe7fc23761f1408
apth3hack3r <67852964+apth3hack3r@users.noreply.github.com>
2020-11-26 11:19:26 +0530

    Add files via upload

---
+++ Language Detector.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from pycountry import languages
+st.title("Language Detector")
+import fasttext
+model = fasttext.load_model('model.bin')
+st.write("Enter text to Detect language.")
+text = st.text_area("")
+if st.button('Detect'):
+	out=model.predict(text,k=2)
+	lang_name_tup=out[0]
+	lang_per_arr=out[1]
+	for i in range(2):
+
+		lang_name = languages.get(alpha_3=(lang_name_tup[i])[9:]).name
+		st.write(lang_name +"\t",end="")
+		st.write(lang_per_arr[i]*100)
