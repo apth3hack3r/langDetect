4f263cc85d1b1fe28d8754d8fa5a88de81cff0ca
Pranav from Mark III <pranav.bhawan@gmail.com>
2021-07-06 23:20:10 +0530

    progress bar done, made some changes to submit_button

--- test.py
+++ test.py
@@ -57,7 +57,7 @@ st.markdown(r' <div style="text-align: center"> '+ st.session_state.desc + '</di
 
 
 def form_callback():
-	if(text_input.lower()==values[st.session_state.key].lower()):
+	if(text_input.lower()==values[st.session_state.key].lower() and submit_button):
 		st.session_state.key=st.session_state.key+1
 		st.session_state.desc=descs[st.session_state.key]
 		st.session_state.img_url=img_urls[st.session_state.key]
