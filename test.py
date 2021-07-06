55681cf5d292358172cba53ee10912b3b636534a
Pranav from Mark III <pranav.bhawan@gmail.com>
2021-07-06 23:51:49 +0530

    key cahnge

--- test.py
+++ test.py
@@ -61,9 +61,9 @@ st.markdown(r' <div style="text-align: center"> '+ st.session_state.desc + '</di
 def form_callback():
 	if(text_input.lower()==values[st.session_state.key].lower()):
 		st.session_state.key=st.session_state.key+1
-		st.session_state.desc=descs[st.session_state.key]
-		st.session_state.img_url=img_urls[st.session_state.key]
-		st.session_state.value=values[st.session_state.key]
+		st.session_state.desc=descs[st.session_state.key+1]
+		st.session_state.img_url=img_urls[st.session_state.key+1]
+		st.session_state.value=values[st.session_state.key+1]
 		st.session_state.score_bar = st.progress(st.session_state.key*10) 
 		st.success("WELL DONE!!!")
 	else:
